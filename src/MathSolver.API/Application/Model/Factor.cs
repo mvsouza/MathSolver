1ae9ab96d4d2f88c00b497828c5259bf1d21154c
Matheus Souza <mvs@mvsouza.com>
2018-09-28 14:39:53 -0300

    Operetion precedence explicit defined by parenteses.

--- src/MathSolver.API/Application/Model/Factor.cs
+++ src/MathSolver.API/Application/Model/Factor.cs
@@ -7,18 +7,96 @@ namespace MathSolver.API.Application.Model
 {
     public class Factor
     {
-        public string Calculus { get; set; }
+        public string Calculus { get; private set; }
+        public IEnumerable<Factor> Factors { get; private set; }
+        public (char desc, double seed, Func<double, double, double> func) Operation { get; private set; }
+        public bool HasOperator
+        {
+            get; private set;
+        }
         public Factor(string calculus)
         {
             Calculus = calculus;
+            BreakIntoFactors();
+
         }
+        private void BreakIntoFactors()
+        {
+            Queue<int> parentesesQueue = new Queue<int>();
+            List<char> operations = new List<char>();
+            List<int> operationsIndexes = new List<int>();
+            var numberIndex = 0;
+            for (int index = 0; index < Calculus.Length; index++)
+            {
+                switch (Calculus[index])
+                {
+                    case '(':
+                        parentesesQueue.Enqueue(index);
+                        numberIndex = index + 1;
+                        break;
+                    case ')':
+                        var startParenteses = parentesesQueue.Dequeue();
+                        numberIndex = index + 1;
+                        break;
+                    case ('*'):
+                        if (parentesesQueue.Count == 0)
+                        {
+                            operations.Add('*');
+                            operationsIndexes.Add(index);
+                            numberIndex = index + 1;
+                        }
+                        break;
+                    case ('+'):
+                        if (parentesesQueue.Count == 0)
+                        {
+                            operations.Add('+');
+                            operationsIndexes.Add(index);
+                            numberIndex = index + 1;
+                        }
+                        break;
+                }
+            }
+            HasOperator = operations.Any();
+            if (HasOperator)
+            {
+                Operation = Precedence.First(p => operations.Contains(p.desc));
+                var factorStart = 0;
+
+                List<Factor> factors = new List<Factor>();
+
+                for (int index = 0; index < operations.Count; index++)
+                {
+                    if (operations[index] != Operation.desc)
+                        continue;
+                    factors.Add(new Factor(Calculus.Substring(factorStart, (operationsIndexes[index]- factorStart))));
+                    factorStart = operationsIndexes[index] + 1;
+                }
+
+                factors.Add(new Factor(Calculus.Substring(factorStart, (Calculus.Length - factorStart))));
+                Factors = factors;
+            }
+        }
+
+        public static IEnumerable<(char desc, double seed, Func<double, double, double> func)> Precedence = new (char desc, double seed, Func<double, double, double> func)[] {
+            ('+',0,(acc, x) => acc + x),
+            ('*',1,(acc, x) => acc * x)
+        };
         public double Solve()
         {
-            if(Calculus.Contains("+"))
-                return Calculus.Split("+").Select(s => new Factor(s).Solve()).Sum();
-            if (Calculus.Contains("*"))
-                return Calculus.Split("*").Select(s => new Factor(s).Solve()).Aggregate(1.0, (acc, x) => acc * x);
-            return int.Parse(Calculus);
+            if (HasOperator)
+                return Factors.Select(f => f.Solve()).Aggregate(Operation.seed, Operation.func);
+            if (Calculus[0] == '(' && Calculus[Calculus.Length-1] == ')')
+            {
+                Calculus = Calculus.Substring(1,Calculus.Length-2);
+            }
+            double value;
+            if (double.TryParse(Calculus, out value)){
+                return value;
+            }
+            return new Factor(Calculus).Solve();
+
+
         }
+
     }
 }
--- test/MathSolver.UnitTest/Application/Model/FactorTest.cs
+++ test/MathSolver.UnitTest/Application/Model/FactorTest.cs
@@ -9,7 +9,15 @@ namespace MathSolver.UnitTest.Application.Model
     public class FactorTest
     {
         [Theory]
-        [InlineData("5+5",10.0)]
+        [InlineData("5", 5.0)]
+        [InlineData("13", 13.0)]
+        public void SingleFactor(string calculus, double result)
+        {
+            var factor = new Factor(calculus);
+            Assert.Equal(result, factor.Solve());
+        }
+        [Theory]
+        [InlineData("5+5", 10.0)]
         [InlineData("5+1", 6.0)]
         [InlineData("5+1+3", 9.0)]
         public void AdditionOfNumbers(string calculus, double result)
@@ -32,6 +40,49 @@ namespace MathSolver.UnitTest.Application.Model
         [InlineData("5*1+3", 8.0)]
         [InlineData("3+5*1*2", 13.0)]
         public void MutiplicationAndAdditionOfNumbers(string calculus, double result)
+        {
+            var factor = new Factor(calculus);
+            Assert.Equal(result, factor.Solve());
+        }
+        [Theory]
+        [InlineData("(5)", 5.0)]
+        [InlineData("(13)", 13.0)]
+        public void SingleFactorWithParenteses(string calculus, double result)
+        {
+            var factor = new Factor(calculus);
+            Assert.Equal(result, factor.Solve());
+        }
+        [Theory]
+        [InlineData("(5+1)", 6.0)]
+        [InlineData("(13+13)", 26.0)]
+        public void AdditionBetweenParenteses(string calculus, double result)
+        {
+            var factor = new Factor(calculus);
+            Assert.Equal(result, factor.Solve());
+        }
+        [Theory]
+        [InlineData("(5)+(1)", 6.0)]
+        [InlineData("(13)+(13)", 26.0)]
+        public void AdditionOfSigleFactoresBetweenParenteses(string calculus, double result)
+        {
+            var factor = new Factor(calculus);
+            Assert.Equal(result, factor.Solve());
+        }
+        [Theory]
+        [InlineData("(5+1)*3", 18.0)]
+        [InlineData("2*(13+13)", 52.0)]
+        public void AdditionPredecending(string calculus, double result)
+        {
+            var factor = new Factor(calculus);
+            Assert.Equal(result, factor.Solve());
+        }
+
+        [Theory]
+        [InlineData("((5+1))*3", 18.0)]
+        [InlineData("((2))*(13+13)", 52.0)]
+        [InlineData("(5)+((1))", 6.0)]
+        [InlineData("((13)+(13))", 26.0)]
+        public void DoubleParenteses(string calculus, double result)
         {
             var factor = new Factor(calculus);
             Assert.Equal(result, factor.Solve());
